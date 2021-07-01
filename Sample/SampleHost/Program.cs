71f4ab920c65441a6f7f0e3f97b8336f1f9aa8e9
Mortaza Ghahremani <mortaza.ghahremani@gmail.com>
2021-07-01 13:51:07 +0430

    init

---
+++ Sample/SampleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
